48dfb6f7151b5dd2675637e28a50e452cffd8e23
Teodor Ande Elstad <teodor@andeelstad.com>
2017-05-09 10:36:54 +0200

    Added api/hello and api/hello/{name}

---
+++ CxApi/Controllers/HelloController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CxApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class HelloController : Controller
+    {
+        // GET api/hello
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello who?";
+        }
+
+        // GET api/hello/world
+        [HttpGet("{name}")]
+        public string Get(string name)
+        {
+            return String.Format("Hello {0}!", name);
+        }
+    }
+}
