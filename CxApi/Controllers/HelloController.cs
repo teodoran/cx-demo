7e8abf692c12c25f88346f4e7bf5cbfd41b2fa55
Teodor Ande Elstad <teodor@andeelstad.com>
2017-05-09 11:04:49 +0200

    Using Json as response type

--- CxApi/Controllers/HelloController.cs
+++ CxApi/Controllers/HelloController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CxApi.Models;
 
 namespace CxApi.Controllers
 {
@@ -11,16 +12,26 @@ namespace CxApi.Controllers
     {
         // GET api/hello
         [HttpGet]
-        public string Get()
+        public JsonResult Get()
         {
-            return "Hello Dr. Who?";
+            var response = new HelloResponse
+            {
+                Prompt = "Hello Dr. Who?" 
+            };
+
+            return Json(response);
         }
 
         // GET api/hello/world
         [HttpGet("{name}")]
-        public string Get(string name)
+        public JsonResult Get(string name)
         {
-            return String.Format("Hello {0}!", name);
+            var response = new HelloResponse
+            {
+                Prompt = String.Format("Hello {0}!", name) 
+            };
+
+            return Json(response);
         }
     }
 }
---
+++ CxApi/Models/HelloResponse.cs
@@ -0,0 +1,7 @@
+namespace CxApi.Models
+{
+    public class HelloResponse
+    {
+        public string Prompt { get; set; }
+    }
+}
